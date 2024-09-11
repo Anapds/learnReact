8b9c99aa635423a235f6275aec6fe66b99f25601
ana <paulati1102@gmail.com>
2024-09-11 20:38:22 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
+  const [base, setBase] = useState();
+  const [altura, setAltura] = useState();
+  const [area, setArea] = useState();
+
+  function calcular(){
+    if (base > 0 && altura > 0) {
+        setArea((parseFloat(base) * parseFloat(altura)) / 2);
+    } else {
+      setArea("Valores de base e altura inválidos");
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <text>Insira os dados abaixo para calcular a área</text>
+      <TextInput
+        placeholder='Base'
+        keyboardType='numeric'
+        onChangeText={(b) => setBase()}
+      />
+      <TextInput
+        placeholder='Altura'
+        keyboardType='numeric'
+        onChangeText={(a) => setAltura(a)}
+      />
+      <button
+        title='calcular'
+        onPress={calcular} />
+        <text>{ area ? `resultado: ${area}` : ''}</text>
     </View>
   );
 }
